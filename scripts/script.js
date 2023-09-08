19b8f4d366f70674ed0548b8148024dc5afb2d72
Larissa <larissa.mikkers@hotmail.com>
2023-09-08 11:42:43 +0200

    Nieuwe visite kaartje
    Had in het verkeerde github opgeslagen hierbij mijn nieuwe ontwerp html css js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,28 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+// const expandButton = document.querySelector('header button')
+// expandButton.addEventListener('click', expand)
 
-function expand () {
-  document.body.classList.toggle('expand')
+// function expand() {
+//     document.body.classList.toggle('expand')
+// }
+// console.log('test G');
+
+
+// var bodySelect = document.querySelector("body");
+
+// --- TOGGLE BUTTON CLASS ---
+// var meerButton = document.querySelector(".toggle");
+// var backButton = document.querySelector(".backBtn");
+
+// wanneer er op de 'meer button' word geclickt gaat de functie toggleButton van gang//
+// meerButton.addEventListener("click", toggleButton);
+// backButton.addEventListener("click", gobackButton);
+
+function turn() {
+  var element = document.getElementById("card");
+  element.classList.toggle('turned');
 }
+
+// function gobackButton() {
+//     console.log('klikt op toggle gegevens button')
+//     bodySelect.classList.remove('showCard');
+// }
